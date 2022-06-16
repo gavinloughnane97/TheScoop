a53565f3f1dfca8bcc992dd9aa272d4ece0d5855
Gavin Loughnane <gavinloughnane97@gmail.com>
2022-06-16 12:17:31 -0400

    added test data

--- gameInfo/apiTester.py
+++ gameInfo/apiTester.py
@@ -1,4 +1,6 @@
 import statsapi
 
-print(statsapi.standings(date='06/13/2022'))
+gameDict = statsapi.schedule(date=None, start_date="2021-04-07", end_date="2022-06-16", team="", opponent="", sportId=1, game_id=None)
+
+print(gameDict)
 
